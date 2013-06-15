0ac7ab1499bfdffc8fd55d5bb9c8af76cf626a72
Leonardo Proietti <leonardo.proietti@gmail.com>
2013-06-15 12:06:14 +0200

    add __get to TranslatableWrapper because with __call only entity form type does not work

--- Model/TranslatableWrapper.php
+++ Model/TranslatableWrapper.php
@@ -4,7 +4,7 @@ namespace PUGX\I18nBundle\Model;
 
 abstract class TranslatableWrapper extends Translatable
 {    
-    public function __call($method, $args)
+    public function __get($method)
     {
         $translation = $this->getTranslation();
         $reflectedTranslation = new \ReflectionClass(get_class($translation));
@@ -14,17 +14,22 @@ abstract class TranslatableWrapper extends Translatable
             if ($reflectedTranslation->hasMethod($getter)) {
                 $reflectedMethod = $reflectedTranslation->getMethod($getter);
                 if ($reflectedMethod->isPublic()) {
-                    return $reflectedMethod->invokeArgs($translation, $args);
+                    return $reflectedMethod->invoke($translation, $getter);
                 }
             }
         }
         
         throw new \RuntimeException(sprintf('The method "%s" does not exist', $method));
     }
+
+    public function __call($method, $args)
+    {
+        return $this->__get($method);
+    }
     
     protected function getGetters($method)
     {
-        $getters = array();        
+        $getters = array();
         $getters[] = $method;
         $getters[] = 'get' . ucfirst($method);
         
