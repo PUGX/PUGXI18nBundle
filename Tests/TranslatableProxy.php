3c78d59e28b74ba5b9c5d9086e85afd9781a6c9b
Leonardo Proietti <leonardo.proietti@gmail.com>
2014-02-21 14:54:37 +0100

    fix test

--- Tests/TranslatableProxy.php
+++ Tests/TranslatableProxy.php
@@ -5,8 +5,20 @@ namespace PUGX\I18nBundle\Tests;
 use Doctrine\ORM\Proxy\Proxy;
 
 class TranslatableProxy extends Translatable implements Proxy
-{    
+{        
     public function __load() { }
 
     public function __isInitialized() { }
+
+    public function __setInitialized($initialized) {}
+
+    public function __setInitializer(Closure $initializer = null) {}
+
+    public function __getInitializer() {}
+
+    public function __setCloner(Closure $cloner = null) {}
+
+    public function __getCloner() {}
+
+    public function __getLazyProperties() {}
 }
