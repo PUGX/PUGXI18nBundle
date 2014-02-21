830c1a328c846d85a1b46755f26f61c4cbb5543d
Giorgio Cefaro <giorgio.cefaro@gmail.com>
2014-02-21 14:58:52 +0100

    minor CS fix

--- Tests/TranslatableProxy.php
+++ Tests/TranslatableProxy.php
@@ -5,7 +5,7 @@ namespace PUGX\I18nBundle\Tests;
 use Doctrine\ORM\Proxy\Proxy;
 
 class TranslatableProxy extends Translatable implements Proxy
-{        
+{
     public function __load() { }
 
     public function __isInitialized() { }
